ce456c98bc07eae4150f89d013eeaa1f3ccc43da
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-05-10 17:28:55 +0100

    package rename

---
+++ app/uk/gov/hmrc/vatsignup/services/IdentityVerificationOrchestrationService.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.services
+
+import javax.inject.{Inject,Singleton}
+
+import cats.data.EitherT
+import cats.implicits._
+import uk.gov.hmrc.auth.core.ConfidenceLevel
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.vatsignup.connectors.IdentityVerificationConnector
+import uk.gov.hmrc.vatsignup.httpparsers
+import uk.gov.hmrc.vatsignup.httpparsers.IdentityVerified
+import uk.gov.hmrc.vatsignup.repositories.SubscriptionRequestRepository
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class IdentityVerificationOrchestrationService @Inject()(subscriptionRequestRepository: SubscriptionRequestRepository,
+                                                         identityVerificationConnector: IdentityVerificationConnector
+                                                        )(implicit ec: ExecutionContext) {
+
+  import uk.gov.hmrc.vatsignup.services.IdentityVerificationOrchestrationService._
+
+  def updateIdentityVerificationState(vatNumber: String,
+                                      journeyLink: String,
+                                      confidenceLevel: ConfidenceLevel
+                                     )(implicit hc: HeaderCarrier): Future[IdentityVerificationOrchestrationResponse] = {
+    for {
+      _ <- checkIdentityVerification(journeyLink, confidenceLevel)
+      _ <- upsertIdentityVerified(vatNumber)
+    } yield IdentityVerified
+  }.value
+
+
+  private def checkIdentityVerification(journeyLink: String,
+                                        confidenceLevel: ConfidenceLevel
+                                       )(implicit hc: HeaderCarrier): EitherT[Future, IdentityVerificationOrchestrationFailure, IdentityVerified.type] =
+    EitherT(if (confidenceLevel < ConfidenceLevel.L200) {
+      identityVerificationConnector.getIdentityVerificationOutcome(journeyLink) map {
+        case Right(IdentityVerified) => Right(IdentityVerified)
+        case Right(httpparsers.IdentityNotVerified(_)) => Left(IdentityNotVerified)
+        case _ => Left(IdentityVerificationConnectionFailure)
+      }
+    } else {
+      Future.successful(Right(IdentityVerified))
+    })
+
+  private def upsertIdentityVerified(vatNumber: String): EitherT[Future, IdentityVerificationOrchestrationFailure, IdentityVerified.type] =
+    EitherT(subscriptionRequestRepository.upsertIdentityVerified(vatNumber)
+      .map(_ => Right(IdentityVerified))
+      .recover { case _ => Left(IdentityVerificationDatabaseFailure) })
+}
+
+object IdentityVerificationOrchestrationService {
+
+  type IdentityVerificationOrchestrationResponse = Either[IdentityVerificationOrchestrationFailure, IdentityVerified.type]
+
+  sealed trait IdentityVerificationOrchestrationFailure
+
+  case object IdentityNotVerified extends IdentityVerificationOrchestrationFailure
+
+  case object IdentityVerificationConnectionFailure extends IdentityVerificationOrchestrationFailure
+
+  case object IdentityVerificationDatabaseFailure extends IdentityVerificationOrchestrationFailure
+
+}
---
+++ it/uk/gov/hmrc/vatsignup/helpers/servicemocks/AuthStub.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.helpers.servicemocks
+
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.HeaderNames
+import play.api.http.Status._
+import play.api.libs.json.{JsObject, Json, Writes}
+import uk.gov.hmrc.auth.core.ConfidenceLevel
+import uk.gov.hmrc.auth.core.ConfidenceLevel.L0
+import uk.gov.hmrc.vatsignup.config.Constants._
+import uk.gov.hmrc.vatsignup.helpers.IntegrationTestConstants._
+
+object AuthStub extends WireMockMethods {
+  val authority = "/auth/authorise"
+
+  def stubAuth[T](status: Int, body: T)(implicit writes: Writes[T]): StubMapping = {
+    when(method = POST, uri = authority)
+      .thenReturn(status = status, body = writes.writes(body))
+  }
+
+  private def exceptionHeaders(value: String) = Map(HeaderNames.WWW_AUTHENTICATE -> s"""MDTP detail="$value"""")
+
+  def stubAuthFailure(): StubMapping = {
+    when(method = POST, uri = authority)
+      .thenReturn(status = UNAUTHORIZED, headers = exceptionHeaders("MissingBearerToken"))
+  }
+
+  def successfulAuthResponse(confidenceLevel: ConfidenceLevel, enrolments: JsObject*): JsObject = Json.obj(
+    "allEnrolments" -> enrolments,
+    "confidenceLevel" -> confidenceLevel.level
+  )
+
+  def successfulAuthResponse(enrolments: JsObject*): JsObject =
+    successfulAuthResponse(L0, enrolments:_*)
+
+  val agentEnrolment: JsObject = Json.obj(
+    "key" -> AgentEnrolmentKey,
+    "identifiers" -> Json.arr(
+      Json.obj(
+        "key" -> AgentReferenceNumberKey,
+        "value" -> testAgentNumber
+      )
+    )
+  )
+
+  val vatDecEnrolment: JsObject = Json.obj(
+    "key" -> VatDecEnrolmentKey,
+    "identifiers" -> Json.arr(
+      Json.obj(
+        "key" -> VatReferenceKey,
+        "value" -> testVatNumber
+      )
+    )
+  )
+}
