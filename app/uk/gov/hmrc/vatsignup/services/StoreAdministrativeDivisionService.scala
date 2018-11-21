d343715e43003b19d6b607a0c30f44bd28243090
r-melvin <richard.melvin@digital.hmrc.gov.uk>
2018-11-21 16:03:51 +0000

    SAR-2910 - New storeAdministrativeDivision API

---
+++ app/uk/gov/hmrc/vatsignup/services/StoreAdministrativeDivisionService.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.services
+
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.vatsignup.models.AdministrativeDivision
+import uk.gov.hmrc.vatsignup.repositories.SubscriptionRequestRepository
+import uk.gov.hmrc.vatsignup.services.StoreAdministrativeDivisionService._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+@Singleton
+class StoreAdministrativeDivisionService @Inject()(subscriptionRequestRepository: SubscriptionRequestRepository)
+                                                  (implicit ec: ExecutionContext) {
+
+  def storeAdministrativeDivision(vatNumber: String)
+                                 (implicit hc: HeaderCarrier): Future[Either[StoreAdministrativeDivisionFailure, StoreAdministrativeDivisionSuccess.type]] = {
+    subscriptionRequestRepository.upsertBusinessEntity(vatNumber, AdministrativeDivision) map {
+      _ => Right(StoreAdministrativeDivisionSuccess)
+    } recover {
+      case e: NoSuchElementException => Left(AdministrativeDivisionDatabaseFailureNoVATNumber)
+      case _ => Left(AdministrativeDivisionDatabaseFailure)
+    }
+  }
+
+}
+
+object StoreAdministrativeDivisionService {
+
+  case object StoreAdministrativeDivisionSuccess
+
+  sealed trait StoreAdministrativeDivisionFailure
+
+  case object AdministrativeDivisionDatabaseFailure extends StoreAdministrativeDivisionFailure
+
+  case object AdministrativeDivisionDatabaseFailureNoVATNumber extends StoreAdministrativeDivisionFailure
+
+}
+
---
+++ test/uk/gov/hmrc/vatsignup/service/StoreAdministrativeDivisionServiceSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.service
+
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import reactivemongo.api.commands.UpdateWriteResult
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsignup.helpers.TestConstants._
+import uk.gov.hmrc.vatsignup.models.AdministrativeDivision
+import uk.gov.hmrc.vatsignup.repositories.mocks.MockSubscriptionRequestRepository
+import uk.gov.hmrc.vatsignup.services.StoreAdministrativeDivisionService
+import uk.gov.hmrc.vatsignup.services.StoreAdministrativeDivisionService._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class StoreAdministrativeDivisionServiceSpec extends UnitSpec
+  with MockSubscriptionRequestRepository {
+
+  object TestStoreAdministrativeDivisionService extends StoreAdministrativeDivisionService(
+    mockSubscriptionRequestRepository
+  )
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+  implicit val request: Request[_] = FakeRequest()
+
+
+  "storeAdministrativeDivision" when {
+    "upsertBusinessEntity is successful" should {
+      "return StoreAdministrativeDivisionSuccess" in {
+        mockUpsertBusinessEntity(testVatNumber, AdministrativeDivision)(Future.successful(mock[UpdateWriteResult]))
+
+        val res = TestStoreAdministrativeDivisionService.storeAdministrativeDivision(testVatNumber)
+
+        await(res) shouldBe Right(StoreAdministrativeDivisionSuccess)
+
+      }
+    }
+    "upsertBusinessEntity returns AdministrativeDivisionDatabaseFailure" should {
+      "return StoreAdministrativeDivisionFailure" in {
+        mockUpsertBusinessEntity(testVatNumber, AdministrativeDivision)(Future.failed(new Exception))
+
+        val res = TestStoreAdministrativeDivisionService.storeAdministrativeDivision(testVatNumber)
+
+        await(res) shouldBe Left(AdministrativeDivisionDatabaseFailure)
+
+      }
+    }
+    "upsertBusinessEntity throws NoSuchElementException" should {
+      "return AdministrativeDivisionDatabaseFailureNoVATNumber" in {
+        mockUpsertBusinessEntity(testVatNumber, AdministrativeDivision)(Future.failed(new NoSuchElementException))
+
+        val res = TestStoreAdministrativeDivisionService.storeAdministrativeDivision(testVatNumber)
+
+        await(res) shouldBe Left(AdministrativeDivisionDatabaseFailureNoVATNumber)
+
+      }
+    }
+  }
+
+}
