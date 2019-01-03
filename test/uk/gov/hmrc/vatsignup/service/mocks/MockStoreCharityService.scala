a3bb9f2683ac277a025ab04f92af155451b6bec9
Jernej Vasic <jernej.vasic@digital.hmrc.gov.uk>
2019-01-03 12:23:42 +0000

    SAR-3114: new charity controller and service

---
+++ test/uk/gov/hmrc/vatsignup/service/mocks/MockStoreCharityService.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.service.mocks
+
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, Suite}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.vatsignup.services.StoreCharityService
+import uk.gov.hmrc.vatsignup.services.StoreCharityService.{StoreCharityFailure, StoreCharitySuccess}
+
+import scala.concurrent.Future
+
+trait MockStoreCharityService extends MockitoSugar with BeforeAndAfterEach {
+  self: Suite =>
+
+  val mockStoreCharityService: StoreCharityService = mock[StoreCharityService]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockStoreCharityService)
+  }
+
+  def mockStoreCharity(vatNumber: String)(response: Future[Either[StoreCharityFailure, StoreCharitySuccess.type]]): Unit = {
+    when(mockStoreCharityService.storeCharity(
+      ArgumentMatchers.eq(vatNumber)
+    )(ArgumentMatchers.any[HeaderCarrier])) thenReturn response
+  }
+
+}
