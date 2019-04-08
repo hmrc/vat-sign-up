7b5c4562211043d6307960c2f39d3e70e197eac4
Rob Hawkes <17453872+kabuumu@users.noreply.github.com>
2019-04-08 13:58:37 +0100

    SAR-3643 - Remove IV check on submission

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,14 +12,14 @@ object AppDependencies {
   private val catsVersion = "1.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.4.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.8.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.10.0",
     "org.typelevel" %% "cats-core" % catsVersion
   )
 
   def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.4.0-play-25" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.6.0-play-25" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
