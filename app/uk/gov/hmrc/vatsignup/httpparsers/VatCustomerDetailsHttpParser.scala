2d32d2239c7054fe3b24e6e35b394212fd98dd40
Rob Hawkes <17453872+kabuumu@users.noreply.github.com>
2019-11-08 13:45:47 +0000

    SAR-4784 - Add new connector to handle overseas flag

---
+++ app/uk/gov/hmrc/vatsignup/httpparsers/VatCustomerDetailsHttpParser.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.httpparsers
+
+import play.api.http.Status._
+import play.api.libs.json.JsSuccess
+import uk.gov.hmrc.http.{HttpReads, HttpResponse}
+import uk.gov.hmrc.vatsignup.httpparsers.KnownFactsHttpParser.KnownFacts
+import uk.gov.hmrc.vatsignup.models.VatCustomerDetails
+
+object VatCustomerDetailsHttpParser {
+  type VatCustomerDetailsHttpParserResponse = Either[VatCustomerDetailsFailure, VatCustomerDetails]
+
+  val postCodeKey = "businessPostCode"
+
+  val registrationDateKey = "vatRegistrationDate"
+
+  val invalidJsonResponseMessage = "Invalid JSON response"
+
+  val isOverseasKey = "isOverseas"
+
+  implicit object VatCustomerDetailsHttpReads extends HttpReads[VatCustomerDetailsHttpParserResponse] {
+    def read(method: String, url: String, response: HttpResponse): VatCustomerDetailsHttpParserResponse = {
+      response.status match {
+        case OK =>
+          (for {
+            businessPostcode <- (response.json \ postCodeKey).validate[String]
+            vatRegistrationDate <- (response.json \ registrationDateKey).validate[String]
+            isOverseas <- (response.json \ isOverseasKey).validate[Boolean]
+          } yield VatCustomerDetails(KnownFacts(businessPostcode, vatRegistrationDate), isOverseas)) match {
+            case JsSuccess(customerDetails, _) =>
+              Right(customerDetails)
+            case _ =>
+              Left(InvalidKnownFacts(
+                status = response.status,
+                body = invalidJsonResponseMessage
+              ))
+           }
+        case BAD_REQUEST =>
+          Left(InvalidVatNumber)
+        case NOT_FOUND =>
+          Left(VatNumberNotFound)
+        case _ =>
+          Left(InvalidKnownFacts(
+            status = response.status,
+            body = response.body
+          ))
+      }
+    }
+  }
+
+  sealed trait VatCustomerDetailsFailure
+
+  case class InvalidKnownFacts(status: Int, body: String) extends VatCustomerDetailsFailure
+
+  case object InvalidVatNumber extends VatCustomerDetailsFailure
+
+  case object VatNumberNotFound extends VatCustomerDetailsFailure
+
+}
---
+++ it/uk/gov/hmrc/vatsignup/connectors/VatCustomerDetailsConnectorISpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.connectors
+
+import org.scalatest.EitherValues
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.vatsignup.helpers.ComponentSpecBase
+import uk.gov.hmrc.vatsignup.helpers.IntegrationTestConstants._
+import uk.gov.hmrc.vatsignup.helpers.servicemocks.KnownFactsStub
+import uk.gov.hmrc.vatsignup.httpparsers.KnownFactsHttpParser.KnownFacts
+import uk.gov.hmrc.vatsignup.httpparsers.VatCustomerDetailsHttpParser._
+import uk.gov.hmrc.vatsignup.models.VatCustomerDetails
+
+class VatCustomerDetailsConnectorISpec extends ComponentSpecBase with EitherValues {
+
+  private lazy val vatCustomerDetailsConnector: VatCustomerDetailsConnector =
+    app.injector.instanceOf[VatCustomerDetailsConnector]
+
+  private implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
+
+  "getVatCustomerDetails" when {
+    "Vat subscription returns a successful response" should {
+      "return the known facts" in {
+        KnownFactsStub.stubSuccessGetKnownFacts(testVatNumber)
+
+        val res = await(vatCustomerDetailsConnector.getVatCustomerDetails(testVatNumber))
+
+        res.right.value shouldBe VatCustomerDetails(KnownFacts(testPostCode, testDateOfRegistration), isOverseas = false)
+      }
+    }
+
+    "Vat subscription returns a successful response for an overseas user" should {
+      "return the known facts" in {
+        KnownFactsStub.stubSuccessGetKnownFactsOverseas(testVatNumber)
+
+        val res = await(vatCustomerDetailsConnector.getVatCustomerDetails(testVatNumber))
+
+        res.right.value shouldBe VatCustomerDetails(KnownFacts(testPostCode, testDateOfRegistration), isOverseas = true)
+      }
+    }
+
+    "Vat subscription returns returns a BAD_REQUEST" should {
+      "return an InvalidVatNumber" in {
+        KnownFactsStub.stubFailureInvalidVatNumber(testVatNumber)
+
+        val res = await(vatCustomerDetailsConnector.getVatCustomerDetails(testVatNumber))
+
+        res.left.value shouldBe InvalidVatNumber
+      }
+    }
+
+    "Vat subscription returns a NOT_FOUND" should {
+      "return a VatNumberNotFound" in {
+        KnownFactsStub.stubFailureVatNumberNotFound(testVatNumber)
+
+        val res = await(vatCustomerDetailsConnector.getVatCustomerDetails(testVatNumber))
+
+        res.left.value shouldBe VatNumberNotFound
+      }
+    }
+  }
+}
