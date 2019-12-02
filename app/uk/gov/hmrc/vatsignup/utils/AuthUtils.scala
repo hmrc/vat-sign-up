e3506d0c135d40b1e36150a35194dd322eb1c9ee
Rob Hawkes <17453872+kabuumu@users.noreply.github.com>
2019-12-02 10:18:05 +0000

    SAR-4914

---
+++ app/uk/gov/hmrc/vatsignup/utils/AuthUtils.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignup.utils
+
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.Base64
+
+import play.api.http.HeaderNames._
+import play.api.mvc.{AnyContent, Request}
+
+import scala.util.matching.Regex
+
+case class BasicAuthentication(username: String, password: String)
+
+object AuthUtils {
+  def decodeFromBase64(encodedString: String): String = new String(Base64.getDecoder.decode(encodedString), UTF_8)
+
+  def encodeToBase64(string: String): String = Base64.getEncoder.encodeToString(string.getBytes(UTF_8))
+
+  def getBasicAuth(request: Request[AnyContent]): Option[BasicAuthentication] = {
+    request.headers.get(AUTHORIZATION) match {
+      case Some(BasicAuthHeader(encodedAuthHeader)) =>
+        decodeFromBase64(encodedAuthHeader) match {
+          case DecodedAuth(username, password) =>
+            Some(BasicAuthentication(username, password))
+          case _ =>
+            None
+        }
+      case _ =>
+        None
+    }
+  }
+
+  val BasicAuthHeader: Regex = "Basic (.+)".r
+  val DecodedAuth: Regex = "(.+):(.+)".r
+}
--- it/uk/gov/hmrc/vatsignup/controllers/BulkMigrationAutoClaimEnrolmentControllerISpec.scala
+++ it/uk/gov/hmrc/vatsignup/controllers/BulkMigrationAutoClaimEnrolmentControllerISpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vatsignup.controllers
 
 import play.api.libs.json.Json
@@ -7,9 +23,7 @@ import uk.gov.hmrc.vatsignup.helpers.servicemocks.{EnrolmentStoreProxyStub, Know
 import uk.gov.hmrc.vatsignup.helpers.{ComponentSpecBase, CustomMatchers}
 import uk.gov.hmrc.vatsignup.utils.KnownFactsDateFormatter.KnownFactsDateFormatter
 
-
-class BulkMIgrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase with CustomMatchers {
-
+class BulkMigrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase with CustomMatchers {
 
   s"/migration-notification/vat-number/$testVatNumber" should {
     "successfully add the enrolment and return NO_CONTENT" in {
@@ -20,7 +34,7 @@ class BulkMIgrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase w
       EnrolmentStoreProxyStub.stubUpsertEnrolment(testVatNumber, testPostCode, testDateOfRegistration.toTaxEnrolmentsFormat)(NO_CONTENT)
       EnrolmentStoreProxyStub.stubAllocateEnrolmentWithoutKnownFacts(testVatNumber, testGroupId, testCredentialId)(CREATED)
 
-      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj())
+      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj(), basicAuthHeader)
       res should have(
         httpStatus(NO_CONTENT)
       )
@@ -31,7 +45,7 @@ class BulkMIgrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase w
 
     "return NO_CONTENT if no group ids are found" in {
       EnrolmentStoreProxyStub.stubGetAllocatedLegacyVatEnrolmentStatus(testVatNumber)(NO_CONTENT)
-      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj())
+      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj(), basicAuthHeader)
       res should have(
         httpStatus(NO_CONTENT)
       )
@@ -41,7 +55,7 @@ class BulkMIgrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase w
       EnrolmentStoreProxyStub.stubGetAllocatedLegacyVatEnrolmentStatus(testVatNumber)(OK)
       EnrolmentStoreProxyStub.stubGetUserId(testVatNumber)(NO_CONTENT)
 
-      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj())
+      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj(), basicAuthHeader)
       res should have(
         httpStatus(NO_CONTENT)
       )
@@ -55,12 +69,21 @@ class BulkMIgrationAutoClaimEnrolmentControllerISpec extends ComponentSpecBase w
       EnrolmentStoreProxyStub.stubUpsertEnrolment(testVatNumber, testPostCode, testDateOfRegistration.toTaxEnrolmentsFormat)(NO_CONTENT)
       EnrolmentStoreProxyStub.stubAllocateEnrolmentWithoutKnownFacts(testVatNumber, testGroupId, testCredentialId)(INTERNAL_SERVER_ERROR)
 
-      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj())
+      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj(), basicAuthHeader)
 
       res should have(
         httpStatus(INTERNAL_SERVER_ERROR)
       )
 
+    }
+
+    "return UNAUTHORIZED if the correct authentication details are not supplied" in {
+      val res = post(s"/migration-notification/vat-number/$testVatNumber")(Json.obj())
+
+      res should have(
+        httpStatus(UNAUTHORIZED),
+        httpHeader(WWW_AUTHENTICATE -> "Basic realm=\"realm\"")
+      )
     }
   }
 }
